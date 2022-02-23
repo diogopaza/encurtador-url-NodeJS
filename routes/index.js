062882e3d1c7f84f1e9493e16c25a1180702f6c6
diogopaza <diogopazacvel@gmail.com>
2022-02-23 13:31:12 -0300

    finished encoding

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,14 @@ var express = require('express');
 const Link = require('../models/link');
 var router = express.Router();
 
-/* GET home page. */
+router.get('/:code/stats', async(req,res,next) => {
+  const code = req.params.code;
+  resultado = await Link.findOne({where: {code}});
+  if (!resultado) return res.sendStatus(404);
+  res.render('stats', resultado.dataValues);
+})
+
+
 router.get('/', function(req, res, next) {  
   res.render('index', { title: 'Encurtador' });
 });
@@ -16,6 +23,16 @@ router.post('/new', async (req, res, next) => {
    code
  }) 
  res.render('stats', resultado.dataValues);
+})
+
+router.get('/:code', async (req, res, next) => {
+  const code = req .params.code;
+  
+  const resultado = await Link.findOne({ where : {code}})
+  resultado.hits++;
+  await resultado.save();
+
+  res.redirect(resultado.url);
 })
   
 
